3d3887dbaaaf364b239c47b84aed1f41b1bbb5b1
xctbtx <xctbtx@DESKTOP-VHL92VH>
2020-08-07 10:06:53 +0700

    CreateModel

---
+++ TMS/TMS.ClassAdminManagement/Model/ClassAdmin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TMS.ClassAdminManagement.Model
+{
+    public class ClassAdmin
+    {
+        [MaxLength(10)]
+        public int ClassAdminId { get; set; }
+        
+        [StringLength(255)]
+        public string FullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public bool Gender { get; set; }
+
+        [StringLength(10)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+        [MaxLength(10)]
+        public int Status { get; set; }
+        [StringLength(255)]
+        public string Remarks { get; set; }
+    }
+}
---
+++ TMS/TMS.ClassAdminManagement/Model/Trainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TMS.ClassAdminManagement.Model
+{
+    public class Trainer
+    {
+        [MaxLength(10)]
+        public int TrainerId { get; set; }
+
+        [StringLength(50)]
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Gender { get; set; }
+
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [MaxLength(10)]
+        public int Stautus { get; set; }
+        [StringLength(255)]
+        public string Remarks { get; set; }
+        public virtual ICollection<ClassBatch> ClassBatches { get; set; }
+    }
+}
