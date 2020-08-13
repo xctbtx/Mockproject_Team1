69680a9531c8168f2c52a0a916f27f953a9b4812
xctbtx <xctbtx@gmail.com>
2020-08-13 10:10:14 +0700

    Create Seed

--- TMS/TMS.ClassAdminManagement/Model/DbInitializer.cs
+++ TMS/TMS.ClassAdminManagement/Model/DbInitializer.cs
@@ -14,26 +14,13 @@ namespace TMS.ClassAdminManagement.Model
 
             var classAdmins = new ClassAdmin[]
             {
-                new ClassAdmin() {Account = "Daisies"},
-                new ClassAdmin() {Account = "Lilies"},
-                new ClassAdmin() {Account = "Orchids"},
-                new ClassAdmin() {Account = "Roses"},
-                new ClassAdmin() {Account = "Sunflower"},
+                new ClassAdmin(){FullName="Aaron Stanley",DateOfBirth =new System.DateTime(1998,03,23),Gender=true,PhoneNumber = "0943116030",Email="suscipit.nonummy@Proin.org",Account="Allistair" },
+                new ClassAdmin(){FullName="Aidan Barron",DateOfBirth =new System.DateTime(1993,4,12),Gender=true,PhoneNumber = "0372369370",Email="elit.pellentesque@nislNulla.co.uk",Account="Boris" },
+                new ClassAdmin(){FullName="Victor Lawrence",DateOfBirth =new System.DateTime(1991,8,2),Gender=false,PhoneNumber = "0928302496",Email="ac@aliquameuaccumsan.co.uk",Account="Yuli" },
+                new ClassAdmin(){FullName="Jonas Larson",DateOfBirth =new System.DateTime(1992,03,01),Gender=true,PhoneNumber = "0352265359",Email="porttitor.eros@sit.co.uk",Account="Murphy" },
+                new ClassAdmin(){FullName="Marshall Holland",DateOfBirth =new System.DateTime(1997,3,25),Gender=false,PhoneNumber = "0802735142",Email="amet.ultricies@massaQuisque.org",Account="Alvin" }
             };
             context.ClassAdmins.AddRange(classAdmins);
-
-            var trainers = new Trainer[]
-            {
-                new Trainer(){}
-            };
-            context.Trainers.AddRange(trainers);
-
-            var classBatches = new ClassBatch[]
-            {
-                new ClassBatch(){}
-            };
-            context.ClassBatches.AddRange(classBatches);
-
             context.SaveChanges();
         }
     }
